37a3d33b0efdf31c33543818577172fa89a58fbe
yaseotoko <59038088+yaseotoko@users.noreply.github.com>
2022-07-01 17:34:37 +0900

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php try{
-    $db = new PDO('mysql:dbname=kawasaki-kaito_studio;host=mysql57.kawasaki-kaito.sakura.ne.jp;charset=utf8','kawasaki-kaito','1111aaaa'); 
+    $db = new PDO('mysql:dbname=kawasaki-kaito_studio;host=mysql57.kawasaki-kaito.sakura.ne.jp;charset=utf8','xxxxxxxx','xxxxxxxx'); 
         }catch(PDOException $e){
             echo 'DB接続エラー:'. $e->getMessage();
         }
